0c6896271da570ebe1c8750dda34a23e37aa053a
Lucas Montecino <lucasmontecino95@gmail.com>
2024-05-07 20:55:47 -0300

    create server api file

---
+++ server/api/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use("/api/initial", (req, res) => {
+  const { user } = req.body;
+  try {
+    res.status(200).json({ message: `Hello ${user}!` });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Internal Server Error" });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server listening on port ${process.env.PORT || 3000}...`);
+});
+
+module.exports = app;
